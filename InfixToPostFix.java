598fc5a72eae8030a558b9335a772b368266af6b
Nandini-reddy23 <159713613+Nandini-reddy23@users.noreply.github.com>
2024-03-16 23:58:56 +0530

    Add files via upload

---
+++ InfixToPostFix.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class InfixToPostFix {
+    public static  int prec(char c){
+        if(c=='+' || c=='-')
+            return 1;
+        
+        else if(c=='*' || c=='/') return 2;
+        else if(c=='^') return 3;
+        return -1;
+    }
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String postfix="";
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char  c=s.charAt(i);
+            if(Character.isLetterOrDigit(c)){
+                postfix+=c;
+            }
+            else if(c=='(') st.push('(');
+            else if(c==')') {
+                while(!st.isEmpty() && st.peek()!='('){
+                    postfix+=st.peek();
+                    st.pop();
+                }
+                st.pop();
+            }
+            else{
+                while(!st.isEmpty() && prec(c)<=prec(st.peek())){
+                    postfix+=st.peek();
+                    st.pop();
+                }
+            
+            st.push(c);}
+        }
+        System.out.println("postfix="+postfix);
+    }
+}
